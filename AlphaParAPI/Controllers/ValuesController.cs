2d9c2f8bfe521fd3cb94d780bf991807b94ccbda
Matardarix <mathieu.carlier@viacesi.fr>
2018-12-11 19:44:51 +0100

    Test 2 [DO NOT MERGE]

--- AlphaParAPI/Controllers/ValuesController.cs
+++ AlphaParAPI/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+// Je test la CI blabalabalabal
 namespace AlphaParAPI.Controllers
 {
     [Route("api/[controller]")]
