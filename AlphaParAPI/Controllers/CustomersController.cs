ac5c3cff683011425e36467faf3c13ee51a3afc4
Guillaume DERAMOUDT <guillaume.deramoudt@viacesi.fr>
2018-12-13 17:28:09 +0100

    Création de la logique derrière les endpoints (CRUD)

--- AlphaParAPI/Controllers/CustomersController.cs
+++ AlphaParAPI/Controllers/CustomersController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlphaParAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,36 +13,75 @@ namespace AlphaParAPI.Controllers
     [Route("api/customers")]
     public class CustomersController : ControllerBase
     {
+        private readonly ModelContext _context;
+
+        public CustomersController(ModelContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/customers
-        [HttpGet]
+        [HttpGet("", Name = "GetCustomers")]
         public ActionResult<List<Customer>> GetCustomers()
         {
             // Return the customers list
-            return null;
+            return _context.Customer.ToList();
         }
 
         // GET api/customers/name
-        [HttpGet("{name}")]
-        public ActionResult<string> GetCustomer(string name)
+        [HttpGet("{name}", Name = "GetCustomer")]
+        public ActionResult<Customer> GetCustomer(Customer customer, string name)
         {
             // Return the specified customer
-            return "Customer";
+            var specifiedCustomer = _context.Customer.First(x => x.Name == name);
+
+            if (specifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            return specifiedCustomer;
         }
 
         // POST api/customers
         [HttpPost]
-        public ActionResult AddCustomer(Customer customer)
+        public ActionResult AddCustomer([FromBody]Customer customer)
         {
             // Create a customer with all information
-            return null;
+            _context.Customer.Add(customer);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         // PUT api/customers/id
         [HttpPut("{id}")]
-        public ActionResult ModifyCustomer(string id, Customer customer)
+        public ActionResult ModifyCustomer(string id, [FromBody]Customer customer)
         {
             // Update the specified customer
-            return null;
+            var specifiedCustomer = _context.Customer.Find(id);
+            if (specifiedCustomer == null)
+            {
+                return NotFound();
+            }
+
+
+            if (customer.Name == null || customer.Phone == null || customer.Siret == null || customer.Email == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                specifiedCustomer.Name = customer.Name;
+                specifiedCustomer.Siret = customer.Siret;
+                specifiedCustomer.Phone = customer.Phone;
+                specifiedCustomer.Email = customer.Email;
+            }
+
+            _context.Customer.Update(specifiedCustomer);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         // DELETE api/customers/id
@@ -48,11 +89,15 @@ namespace AlphaParAPI.Controllers
         public IActionResult DeleteCustomer(string id)
         {
             // Delete the specified customer
-            return null;
-        }
-    }
+            var specifiedCustomer = _context.Customer.Find(id);
+            if (specifiedCustomer == null)
+            {
+                return NotFound();
+            }
 
-    public class Customer
-    {
+            _context.Customer.Remove(specifiedCustomer);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
--- AlphaParAPI/Controllers/PlansController.cs
+++ AlphaParAPI/Controllers/PlansController.cs
@@ -2,45 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlphaParAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AlphaParAPI.Controllers
 {
     [Route("api/plans")]
-    public class PlansController : Controller
+    public class PlansController : ControllerBase
     {
+        private readonly ModelContext _context;
+
+        public PlansController(ModelContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/plans
-        [HttpGet]
+        [HttpGet("", Name = "GetPlans")]
         public ActionResult<List<Plan>> GetPlans()
         {
             // Return the plans list
-            return null;
+            return _context.Plan.ToList();
         }
 
         // GET api/plans/id
-        [HttpGet("{id}")]
-        public ActionResult<string> GetPlan(string id)
+        [HttpGet("{id}", Name = "GetPlan")]
+        public ActionResult<Plan> GetPlan(string id)
         {
             // Return the specified plan
-            return "Plan";
+            var specifiedPlan = _context.Plan.Find(id);
+
+            if (specifiedPlan == null)
+            {
+                return NotFound();
+            }
+
+            return specifiedPlan;
         }
 
         // POST api/plans
         [HttpPost]
-        public ActionResult AddPlan(Plan plan)
+        public ActionResult AddPlan([FromBody]Plan plan)
         {
             // Create the plan with all information
-            return null;
+            _context.Plan.Add(plan);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         // PUT api/plans/id
         [HttpPut("{id}")]
-        public ActionResult ModifyPlan(string id, Plan plan)
+        public ActionResult ModifyPlan(string id, [FromBody]Plan plan)
         {
             // Update the specified plan
-            return null;
+            var specifiedPlan = _context.Plan.Find(id);
+            if (specifiedPlan == null)
+            {
+                return NotFound();
+            }
+
+
+            if (plan.Name == null || plan.IdPiece == null || plan.Time == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                specifiedPlan.Name = plan.Name;
+                specifiedPlan.IdPiece = plan.IdPiece;
+                specifiedPlan.Time = plan.Time;
+                specifiedPlan.Piece = plan.Piece;
+            }
+
+            _context.Plan.Update(specifiedPlan);
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         // DELETE api/plans/id
@@ -48,11 +89,15 @@ namespace AlphaParAPI.Controllers
         public IActionResult DeletePlan(string id)
         {
             // Delete the specified plan
-            return null;
-        }
-    }
+            var specifiedPlan = _context.Plan.Find(id);
+            if (specifiedPlan == null)
+            {
+                return NotFound();
+            }
 
-    public class Plan
-    {
+            _context.Plan.Remove(specifiedPlan);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 }
