01a0a91c5a720e44507c309c302279de48049bf5
Araxar <gandossi.luigi@gmail.com>
2018-12-18 11:46:33 +0100

    a

--- AlphaParAPI/Models/BaseEntity.cs
+++ AlphaParAPI/Models/BaseEntity.cs
@@ -6,6 +6,6 @@ namespace AlphaParAPI.Models
     public abstract class BaseEntity
     {
         [StringLength(36,MinimumLength = 36, ErrorMessage = "L'Id n'a pas la bonne synthaxe")]
-        public string Id { get; set; }
+        public string Id { get; set; } 
     }
 }
