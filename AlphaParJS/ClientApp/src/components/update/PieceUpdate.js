880132337cd87656b867125b1b511c8607bfd2b5
Soft55 <guillaume.deramoudt@viacesi.fr>
2019-01-10 10:38:02 +0100

    Update PieceUpdate.js

--- AlphaParJS/ClientApp/src/components/update/PieceUpdate.js
+++ AlphaParJS/ClientApp/src/components/update/PieceUpdate.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-
+import { ApiUrl, RedirectUrl } from './../../App';
 
 export class PieceUpdate extends Component {
     displayName = PieceUpdate.name
@@ -11,8 +11,8 @@ export class PieceUpdate extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
 
         this.state = {
-            targetUrl: 'http://localhost:64156/api/pieces/',
-            redirectUrl: 'https://localhost:44335/pieces/',
+            targetUrl: ApiUrl + 'pieces/',
+            redirectUrl: RedirectUrl + 'pieces/',
             currentPiece: [{
                 name: '',
                 stock: 0,
